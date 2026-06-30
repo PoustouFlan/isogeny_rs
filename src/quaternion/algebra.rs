1a13c60ff77c628d567626ffe5791277af81a7e9
Quentin Rataud <quentin.rataud@irisa.fr>
2026-06-30 16:40:08 +0200

    quaternion: refacto algebra.rs

--- src/quaternion/algebra.rs
+++ src/quaternion/algebra.rs
@@ -1,7 +1,11 @@
 use core::ops::{Add, Div, Mul, Neg, Rem, Sub};
 use core::fmt::Debug;
+use core::marker::PhantomData;
 
-/// Generic interface to allow switching between num_bigint, rug, or crypto_bigint.
+// ========================================================================
+// Generic BigInt Interface
+// ========================================================================
+
 pub trait BigIntAlg:
     Clone
     + Debug
@@ -15,180 +19,386 @@ pub trait BigIntAlg:
     + Rem<Output = Self>
     + Neg<Output = Self>
     + Sized
+    + 'static
 {
     fn zero() -> Self;
     fn one() -> Self;
     fn from_i32(val: i32) -> Self;
-
     fn gcd(&self, other: &Self) -> Self;
     fn xgcd(&self, other: &Self) -> (Self, Self, Self);
-
     fn abs(&self) -> Self;
     fn is_zero(&self) -> bool;
 }
 
+// ========================================================================
+// Algebra Configuration (Zero-Sized Types)
+// ========================================================================
+
+/// Trait to securely bind a static prime `p` to a specific algebra type
+pub trait QuatConfig<T: BigIntAlg> {
+    fn p() -> &'static T;
+}
+
+// ========================================================================
+// Integer Quaternions (Numerator Only)
+// ========================================================================
+
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct QuatAlg<T: BigIntAlg> {
-    pub p: T,
+pub struct IntQuat<T: BigIntAlg, P: QuatConfig<T>> {
+    pub coords: [T; 4],
+    _marker: PhantomData<P>,
 }
 
-impl<T: BigIntAlg> QuatAlg<T> {
-    pub fn new(p: T) -> Self {
-        Self { p }
-    }
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct QuatElem<T: BigIntAlg> {
-    pub denom: T,
-    pub coord: [T; 4],
-}
-
-impl<T: BigIntAlg> QuatElem<T> {
-    pub fn new_i32(denom: i32, c0: i32, c1: i32, c2: i32, c3: i32) -> Self {
-        debug_assert!(denom != 0, "QuatElem initialized with zero denominator");
+impl<T: BigIntAlg, P: QuatConfig<T>> IntQuat<T, P> {
+    pub fn new(c0: T, c1: T, c2: T, c3: T) -> Self {
         Self {
-            denom: T::from_i32(denom),
-            coord: [
-                T::from_i32(c0),
-                T::from_i32(c1),
-                T::from_i32(c2),
-                T::from_i32(c3),
-            ],
+            coords: [c0, c1, c2, c3],
+            _marker: PhantomData,
         }
     }
 
-    pub fn from_scalar(numerator: T, denominator: T) -> Self {
-        debug_assert!(!denominator.is_zero(), "QuatElem initialized with zero denominator");
-        Self {
-            denom: denominator,
-            coord: [numerator, T::zero(), T::zero(), T::zero()],
-        }
+    pub fn new_i32(c0: i32, c1: i32, c2: i32, c3: i32) -> Self {
+        Self::new(T::from_i32(c0), T::from_i32(c1), T::from_i32(c2), T::from_i32(c3))
+    }
+
+    pub fn zero() -> Self {
+        Self::new_i32(0, 0, 0, 0)
     }
 
     pub fn is_zero(&self) -> bool {
-        self.coord[0].is_zero()
-            && self.coord[1].is_zero()
-            && self.coord[2].is_zero()
-            && self.coord[3].is_zero()
+        // TODO: make that constant time
+        self.coords[0].is_zero()
+            && self.coords[1].is_zero()
+            && self.coords[2].is_zero()
+            && self.coords[3].is_zero()
     }
 
     pub fn conj(&self) -> Self {
         Self {
-            denom: self.denom.clone(),
-            coord: [
-                self.coord[0].clone(),
-                -self.coord[1].clone(),
-                -self.coord[2].clone(),
-                -self.coord[3].clone(),
+            coords: [
+                self.coords[0].clone(),
+                -self.coords[1].clone(),
+                -self.coords[2].clone(),
+                -self.coords[3].clone(),
             ],
+            _marker: PhantomData,
         }
     }
 
-    /// Aligns denominators of two elements. Returns (scaled_a, scaled_b).
-    fn equal_denom(a: &Self, b: &Self) -> (Self, Self) {
-        debug_assert!(!a.denom.is_zero(), "a.denom is zero in equal_denom");
-        debug_assert!(!b.denom.is_zero(), "b.denom is zero in equal_denom");
-
-        let gcd = a.denom.gcd(&b.denom);
-        let a_mult = b.denom.clone() / gcd.clone();
-        let b_mult = a.denom.clone() / gcd;
-
-        let mut res_a = a.clone();
-        let mut res_b = b.clone();
-
-        for i in 0..4 {
-            res_a.coord[i] = res_a.coord[i].clone() * a_mult.clone();
-            res_b.coord[i] = res_b.coord[i].clone() * b_mult.clone();
+    pub fn scalar_mul(&self, scalar: &T) -> Self {
+        Self {
+            coords: [
+                self.coords[0].clone() * scalar.clone(),
+                self.coords[1].clone() * scalar.clone(),
+                self.coords[2].clone() * scalar.clone(),
+                self.coords[3].clone() * scalar.clone(),
+            ],
+            _marker: PhantomData,
         }
-
-        let common_denom = a.denom.clone() * a_mult;
-        res_a.denom = common_denom.clone();
-        res_b.denom = common_denom;
-
-        (res_a, res_b)
     }
 
-    pub fn add(&self, other: &Self) -> Self {
-        let (mut a_scaled, b_scaled) = Self::equal_denom(self, other);
-        for i in 0..4 {
-            a_scaled.coord[i] = a_scaled.coord[i].clone() + b_scaled.coord[i].clone();
-        }
-        a_scaled
+    pub fn norm(&self) -> T {
+        let conj_x = self.conj();
+        // Multiplication implicitly uses P::p() via the overloaded operator
+        // TODO: faster, direct computation
+        let norm_elem = self * &conj_x;
+        norm_elem.coords[0].clone().abs()
     }
+}
 
-    pub fn sub(&self, other: &Self) -> Self {
-        let (mut a_scaled, b_scaled) = Self::equal_denom(self, other);
-        for i in 0..4 {
-            a_scaled.coord[i] = a_scaled.coord[i].clone() - b_scaled.coord[i].clone();
+// --- IntQuat Standard Operators ---
+
+impl<'a, 'b, T: BigIntAlg, P: QuatConfig<T>> Add<&'b IntQuat<T, P>> for &'a IntQuat<T, P> {
+    type Output = IntQuat<T, P>;
+    fn add(self, rhs: &'b IntQuat<T, P>) -> Self::Output {
+        IntQuat {
+            coords: [
+                self.coords[0].clone() + rhs.coords[0].clone(),
+                self.coords[1].clone() + rhs.coords[1].clone(),
+                self.coords[2].clone() + rhs.coords[2].clone(),
+                self.coords[3].clone() + rhs.coords[3].clone(),
+            ],
+            _marker: PhantomData,
         }
-        a_scaled
     }
+}
 
-    #[inline]
-    fn coord_mul(a: &[T; 4], b: &[T; 4], p: &T) -> [T; 4] {
-        // c0 = a0b0 - a1b1 - p(a2b2 + a3b3)
+impl<'a, 'b, T: BigIntAlg, P: QuatConfig<T>> Sub<&'b IntQuat<T, P>> for &'a IntQuat<T, P> {
+    type Output = IntQuat<T, P>;
+    fn sub(self, rhs: &'b IntQuat<T, P>) -> Self::Output {
+        IntQuat {
+            coords: [
+                self.coords[0].clone() - rhs.coords[0].clone(),
+                self.coords[1].clone() - rhs.coords[1].clone(),
+                self.coords[2].clone() - rhs.coords[2].clone(),
+                self.coords[3].clone() - rhs.coords[3].clone(),
+            ],
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'a, 'b, T: BigIntAlg, P: QuatConfig<T>> Mul<&'b IntQuat<T, P>> for &'a IntQuat<T, P> {
+    type Output = IntQuat<T, P>;
+    fn mul(self, rhs: &'b IntQuat<T, P>) -> Self::Output {
+        let p = P::p();
+        let a = &self.coords;
+        let b = &rhs.coords;
+
         let mut c0 = a[0].clone() * b[0].clone() - a[1].clone() * b[1].clone();
         c0 = c0 - p.clone() * (a[2].clone() * b[2].clone() + a[3].clone() * b[3].clone());
 
-        // c1 = p(a2b3 - a3b2) + a0b1 + a1b0
         let mut c1 = p.clone() * (a[2].clone() * b[3].clone() - a[3].clone() * b[2].clone());
         c1 = c1 + a[0].clone() * b[1].clone() + a[1].clone() * b[0].clone();
 
-        // c2 = a0b2 + a2b0 - a1b3 + a3b1
         let c2 = a[0].clone() * b[2].clone() + a[2].clone() * b[0].clone()
             - a[1].clone() * b[3].clone() + a[3].clone() * b[1].clone();
 
-        // c3 = a0b3 + a3b0 - a2b1 + a1b2
         let c3 = a[0].clone() * b[3].clone() + a[3].clone() * b[0].clone()
             - a[2].clone() * b[1].clone() + a[1].clone() * b[2].clone();
 
-        [c0, c1, c2, c3]
+        IntQuat {
+            coords: [c0, c1, c2, c3],
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'a, T: BigIntAlg, P: QuatConfig<T>> Neg for &'a IntQuat<T, P> {
+    type Output = IntQuat<T, P>;
+    fn neg(self) -> Self::Output {
+        IntQuat {
+            coords: [
+                -self.coords[0].clone(),
+                -self.coords[1].clone(),
+                -self.coords[2].clone(),
+                -self.coords[3].clone(),
+            ],
+            _marker: PhantomData,
+        }
+    }
+}
+
+// ========================================================================
+// Rational Quaternions (Integer + Denominator)
+// ========================================================================
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RatQuat<T: BigIntAlg, P: QuatConfig<T>> {
+    pub num: IntQuat<T, P>,
+    pub denom: T,
+}
+
+impl<T: BigIntAlg, P: QuatConfig<T>> RatQuat<T, P> {
+    pub fn new(num: IntQuat<T, P>, denom: T) -> Self {
+        debug_assert!(!denom.is_zero(), "RatQuat initialized with zero denominator");
+        Self { num, denom }
     }
 
-    pub fn mul(&self, other: &Self, alg: &QuatAlg<T>) -> Self {
-        let denom = self.denom.clone() * other.denom.clone();
-        let coord = Self::coord_mul(&self.coord, &other.coord, &alg.p);
-
-        Self { denom, coord }
+    pub fn new_i32(denom: i32, c0: i32, c1: i32, c2: i32, c3: i32) -> Self {
+        debug_assert!(denom != 0, "RatQuat initialized with zero denominator");
+        Self {
+            num: IntQuat::new_i32(c0, c1, c2, c3),
+            denom: T::from_i32(denom),
+        }
     }
 
-    /// Returns (numerator, denominator) of the reduced norm.
-    pub fn norm(&self, alg: &QuatAlg<T>) -> (T, T) {
-        let conj_x = self.conj();
-        let norm_elem = self.mul(&conj_x, alg);
+    pub fn is_zero(&self) -> bool {
+        self.num.is_zero()
+    }
 
-        let num = norm_elem.coord[0].clone();
-        let denom = norm_elem.denom.clone();
+    pub fn conj(&self) -> Self {
+        Self {
+            num: self.num.conj(),
+            denom: self.denom.clone(),
+        }
+    }
 
-        let g = num.gcd(&denom);
-        let res_num = (num / g.clone()).abs();
-        let res_denom = (denom / g).abs();
+    pub fn norm(&self) -> (T, T) {
+        let num_norm = self.num.norm();
+        let denom_sq = self.denom.clone() * self.denom.clone();
+
+        let g = num_norm.gcd(&denom_sq);
+        let res_num = (num_norm / g.clone()).abs();
+        let res_denom = (denom_sq / g).abs();
 
         (res_num, res_denom)
     }
 
     pub fn normalize(&mut self) {
-        debug_assert!(!self.denom.is_zero(), "Denominator is zero during normalize");
+        debug_assert!(!self.denom.is_zero(), "RatQuat::normalize called on zero denominator");
 
-        let mut g = self.coord[0].clone();
+        let mut g = self.num.coords[0].clone();
         for i in 1..4 {
-            g = g.gcd(&self.coord[i]);
+            g = g.gcd(&self.num.coords[i]);
         }
         g = g.gcd(&self.denom);
 
         let mut sign = T::one();
-
-        // TODO: make that constant time
         if self.denom < T::zero() {
             sign = T::from_i32(-1);
         }
 
         let divisor = g * sign;
+        debug_assert!(!divisor.is_zero(), "RatQuat::normalize encountered zero divisor during reduction");
 
         for i in 0..4 {
-            self.coord[i] = self.coord[i].clone() / divisor.clone();
+            self.num.coords[i] = self.num.coords[i].clone() / divisor.clone();
         }
         self.denom = self.denom.clone() / divisor;
+    }
+
+    // --- Unnormalized (Lazy) Operations ---
+
+    pub fn add_lazy(&self, other: &Self) -> Self {
+        let gcd = self.denom.gcd(&other.denom);
+        let a_mult = other.denom.clone() / gcd.clone();
+        let b_mult = self.denom.clone() / gcd;
+
+        let a_scaled = self.num.scalar_mul(&a_mult);
+        let b_scaled = other.num.scalar_mul(&b_mult);
+
+        Self {
+            num: &a_scaled + &b_scaled,
+            denom: self.denom.clone() * a_mult,
+        }
+    }
+
+    pub fn sub_lazy(&self, other: &Self) -> Self {
+        let gcd = self.denom.gcd(&other.denom);
+        let a_mult = other.denom.clone() / gcd.clone();
+        let b_mult = self.denom.clone() / gcd;
+
+        let a_scaled = self.num.scalar_mul(&a_mult);
+        let b_scaled = other.num.scalar_mul(&b_mult);
+
+        Self {
+            num: &a_scaled - &b_scaled,
+            denom: self.denom.clone() * a_mult,
+        }
+    }
+
+    pub fn mul_lazy(&self, other: &Self) -> Self {
+        Self {
+            num: &self.num * &other.num,
+            denom: self.denom.clone() * other.denom.clone(),
+        }
+    }
+}
+
+// --- RatQuat Standard Operators (Implicitly Normalized) ---
+
+impl<'a, 'b, T: BigIntAlg, P: QuatConfig<T>> Add<&'b RatQuat<T, P>> for &'a RatQuat<T, P> {
+    type Output = RatQuat<T, P>;
+    fn add(self, rhs: &'b RatQuat<T, P>) -> Self::Output {
+        let mut res = self.add_lazy(rhs);
+        res.normalize();
+        res
+    }
+}
+
+impl<'a, 'b, T: BigIntAlg, P: QuatConfig<T>> Sub<&'b RatQuat<T, P>> for &'a RatQuat<T, P> {
+    type Output = RatQuat<T, P>;
+    fn sub(self, rhs: &'b RatQuat<T, P>) -> Self::Output {
+        let mut res = self.sub_lazy(rhs);
+        res.normalize();
+        res
+    }
+}
+
+impl<'a, 'b, T: BigIntAlg, P: QuatConfig<T>> Mul<&'b RatQuat<T, P>> for &'a RatQuat<T, P> {
+    type Output = RatQuat<T, P>;
+    fn mul(self, rhs: &'b RatQuat<T, P>) -> Self::Output {
+        let mut res = self.mul_lazy(rhs);
+        res.normalize();
+        res
+    }
+}
+
+
+// ========================================================================
+// Backend Implementations
+// ========================================================================
+
+pub mod backends {
+    use super::BigIntAlg;
+
+    // --- num_bigint Backend ---
+    use num_bigint::BigInt;
+    use num_integer::Integer;
+    use num_traits::{Signed, Zero, One};
+
+    impl BigIntAlg for BigInt {
+        fn zero() -> Self { <BigInt as Zero>::zero() }
+        fn one() -> Self { <BigInt as One>::one() }
+        fn from_i32(val: i32) -> Self { BigInt::from(val) }
+        fn gcd(&self, other: &Self) -> Self { self.extended_gcd(other).gcd }
+        fn xgcd(&self, other: &Self) -> (Self, Self, Self) {
+            let res = self.extended_gcd(other);
+            (res.gcd, res.x, res.y)
+        }
+        fn abs(&self) -> Self { <BigInt as Signed>::abs(self) }
+        fn is_zero(&self) -> bool { <BigInt as Zero>::is_zero(self) }
+    }
+
+    // --- crypto_bigint Backend ---
+    use crypto_bigint::{Int, NonZero};
+    use core::ops::{Add, Div, Mul, Neg, Rem, Sub};
+
+    #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct CryptoInt<const LIMBS: usize>(pub Int<LIMBS>);
+
+    impl<const LIMBS: usize> Add for CryptoInt<LIMBS> {
+        type Output = Self;
+        fn add(self, rhs: Self) -> Self::Output { CryptoInt(self.0 + rhs.0) }
+    }
+
+    impl<const LIMBS: usize> Sub for CryptoInt<LIMBS> {
+        type Output = Self;
+        fn sub(self, rhs: Self) -> Self::Output { CryptoInt(self.0 - rhs.0) }
+    }
+
+    impl<const LIMBS: usize> Mul for CryptoInt<LIMBS> {
+        type Output = Self;
+        fn mul(self, rhs: Self) -> Self::Output { CryptoInt(self.0 * rhs.0) }
+    }
+
+    impl<const LIMBS: usize> Div for CryptoInt<LIMBS> {
+        type Output = Self;
+        fn div(self, rhs: Self) -> Self::Output {
+            let nz: Option<NonZero<Int<LIMBS>>> = NonZero::new(rhs.0).into();
+            let res: Option<Int<LIMBS>> = (self.0 / nz.expect("Division by zero in CryptoInt::div")).into();
+            CryptoInt(res.expect("Division overflowed"))
+        }
+    }
+
+    impl<const LIMBS: usize> Rem for CryptoInt<LIMBS> {
+        type Output = Self;
+        fn rem(self, rhs: Self) -> Self::Output {
+            let nz: Option<NonZero<Int<LIMBS>>> = NonZero::new(rhs.0).into();
+            let res: Option<Int<LIMBS>> = (self.0 % nz.expect("Division by zero in CryptoInt::rem")).into();
+            CryptoInt(res.expect("Remainder overflowed"))
+        }
+    }
+
+    impl<const LIMBS: usize> Neg for CryptoInt<LIMBS> {
+        type Output = Self;
+        fn neg(self) -> Self::Output { CryptoInt(Int::ZERO - self.0) }
+    }
+
+    impl<const LIMBS: usize> BigIntAlg for CryptoInt<LIMBS> {
+        fn zero() -> Self { CryptoInt(Int::ZERO) }
+        fn one() -> Self { CryptoInt(Int::ONE) }
+        fn from_i32(val: i32) -> Self { CryptoInt(Int::from_i32(val)) }
+        fn gcd(&self, other: &Self) -> Self {
+            let g_uint = self.0.abs().gcd_vartime(&other.0.abs());
+            CryptoInt(Int::new(g_uint.into()))
+        }
+        fn xgcd(&self, other: &Self) -> (Self, Self, Self) {
+            let out = self.0.xgcd(&other.0);
+            (CryptoInt(Int::new(out.gcd.into())), CryptoInt(out.x), CryptoInt(out.y))
+        }
+        fn abs(&self) -> Self { CryptoInt(Int::new(self.0.abs().into())) }
+        fn is_zero(&self) -> bool { self.0.is_zero().into() }
     }
 }
